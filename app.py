7f64c3b883fb473848af9e8ce247336e2953704d
whatsappwai <bi.seguros.siniestros@gmail.com>
2025-08-27 14:43:26 -0500

    Update app.py
    “fix port for render”

--- app.py
+++ app.py
@@ -53,5 +53,9 @@ def verificar():
         return request.args.get("hub.challenge")
     return "Error de verificación", 403
 
+import os
+
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
